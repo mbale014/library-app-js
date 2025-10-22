e498ac917c71ef5417dd57b830ba53d8caa3965d
mbale014 <195115650+mbale014@users.noreply.github.com>
2025-10-22 12:15:56 +0800

    feat: implement book constructor and function add book to library

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+// Book object constructor
+function Book(title, author, year, pages, hasSeen) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+    this.hasSeen = hasSeen;
+
+}
+
+// Add book to library function
+function addBookToLibrary() {
+    const newBook = new Book() ;
+}

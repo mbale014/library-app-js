c35a760c161e276a0944d882fa2c8015b5c96081
mbale014 <195115650+mbale014@users.noreply.github.com>
2025-10-22 16:48:10 +0800

    fix: remove card placeholder, feat: implement function to loop through each books in library and display on page

--- script.js
+++ script.js
@@ -23,6 +23,28 @@ function addBookToLibrary(bookTitle, bookAuthor, bookYear, bookPages, bookhasRea
     myLibrary.push(newBook);
 }
 
+const cards = document.querySelector('.cards');
+
 myLibrary.forEach(book => {
+    const card = document.createElement('div');
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const year = document.createElement('p');
+    const pages = document.createElement('p');
+    const reading = document.createElement('p');
+    const statusReading = book.hasRead ? 'Completed' : 'Reading';
+
+    title.innerText = `${book.title}`;
+    author.innerText = `Author: ${book.author}`;
+    year.innerText = `Year: ${book.year}`;
+    pages.innerText = `${book.pages} pages`;
+    reading.innerText = `${statusReading}`
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(year);
+    card.appendChild(pages);
+    card.appendChild(reading);
+    cards.appendChild(card)
     
 })
